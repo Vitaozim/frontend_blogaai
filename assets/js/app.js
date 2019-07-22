13700897fb3931abd8f52e2bc8ee5656fdb156b1
Victor <xanadu@Xanadu.ciar.ufg.br>
2019-07-22 17:25:26 -0300

    progresso responsividade

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,15 +15,42 @@ jQuery(document).ready(function($) {
 	});
 
 
+	// bxSlider
+
+	// var widthJanela = $(window).width();
+
+	// var nSlidesDicas = 2;
+	// if (widthJanela < ) {}
 
 
 	var $slider_dicas = $("section.dicas-para-blogs .slider-dicas");
 	$slider_dicas.bxSlider({
+		touchEnabled: false,
+		controls: false,
 		slideSelector: $('.item-dica'),
-		slideWidth: 2000,
-		slideMargin: 15,
+		slideWidth: 1000,
+		slideMargin: 0,
 		minSlides: 2,
 		maxSlides: 2,
-		moveSlides: 2,
+		moveSlides: 1,
 	});
+
+
+	// var nSlidesDicas = 2;
+	// if (widthJanela < ) {}
+
+	var $slider_logos_midias = $("section.na-midia ul");
+	$slider_logos_midias.bxSlider({
+		auto: true,
+		pause: 2000,
+		autoStart: true,
+		touchEnabled: false,
+		pager: false,
+		controls: false,
+		slideWidth: 200,
+		slideMargin: 30,
+		minSlides: 6,
+		maxSlides: 6,
+		moveSlides: 1,
+	})
 });
