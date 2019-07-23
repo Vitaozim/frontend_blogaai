a6ce44e99e6089b148cec86e39510603f1a6d72a
Victor <xanadu@Xanadu.ciar.ufg.br>
2019-07-23 16:59:26 -0300

    tentativa de debugar bug no js

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,16 +24,21 @@ jQuery(document).ready(function($) {
 
 
 	var $slider_dicas = $("section.dicas-para-blogs .slider-dicas");
-	if ($slider_dicas.length > 0) $slider_dicas.bxSlider({
-		touchEnabled: false,
-		controls: false,
-		slideSelector: $('.item-dica'),
-		slideWidth: 1000,
-		slideMargin: 0,
-		minSlides: nSlidesDicas,
-		maxSlides: nSlidesDicas,
-		moveSlides: 1,
-	});
+	if ($slider_dicas.length > 0){
+		 $slider_dicas.bxSlider({
+			touchEnabled: false,
+			controls: false,
+			slideSelector: $('.item-dica'),
+			slideWidth: 1000,
+			slideMargin: 0,
+			minSlides: nSlidesDicas,
+			maxSlides: nSlidesDicas,
+			moveSlides: 1,
+		});
+	} else{
+		console.log("não existe o elemento html do slide de dicas, portanto o slider não gerado.")
+	}
+
 
 
 	var nSlidesMarcas = 6;
@@ -42,17 +47,22 @@ jQuery(document).ready(function($) {
 	if (widthJanela < 400) {nSlidesMarcas = 2}
 
 	var $slider_logos_midias = $("section.na-midia ul");
-	if ($slider_logos_midias.length > 0) $slider_logos_midias.bxSlider({
-		auto: true,
-		pause: 2000,
-		autoStart: true,
-		touchEnabled: false,
-		pager: false,
-		controls: false,
-		slideWidth: 200,
-		slideMargin: 30,
-		minSlides: nSlidesMarcas,
-		maxSlides: nSlidesMarcas,
-		moveSlides: 1,
-	})
+	if ($slider_logos_midias.length > 0) {
+		$slider_logos_midias.bxSlider({
+			auto: true,
+			pause: 2000,
+			autoStart: true,
+			touchEnabled: false,
+			pager: false,
+			controls: false,
+			slideWidth: 200,
+			slideMargin: 30,
+			minSlides: nSlidesMarcas,
+			maxSlides: nSlidesMarcas,
+			moveSlides: 1,
+		});
+	} else{
+		console.log("não existe o elemento html do slide de logos, portanto o slider não gerado.")
+	}
+
 });
